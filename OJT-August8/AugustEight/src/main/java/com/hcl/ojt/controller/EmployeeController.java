6f2a1b487185eeddaedd95399cba327c1c736a65
sahilraoyadav <50744190+sahilraoyadav@users.noreply.github.com>
2021-08-09 02:45:11 -0400

    Added Logging

--- OJT-August8/AugustEight/src/main/java/com/hcl/ojt/controller/EmployeeController.java
+++ OJT-August8/AugustEight/src/main/java/com/hcl/ojt/controller/EmployeeController.java
@@ -15,34 +15,48 @@ import com.hcl.ojt.entity.Employee;
 import com.hcl.ojt.entity.EmployeeDao;
 import com.hcl.ojt.service.EmployeeService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class EmployeeController {
 	@Autowired
 	EmployeeService es;
+
 	@GetMapping(value = "/")
 	public String showEmployees() {
+		log.trace("showEmployees start page of the app");
 		return "Welcome /createEmployees to create employees, /getEmployees/id to get employee, /deleteEmployees/id to delete employee";
 	}
-	@PostMapping(value="/createEmployees")
+
+	@PostMapping(value = "/createEmployees")
 	public Employee createEmployee(@RequestBody EmployeeDao dao) {
+		log.info("employee created " + dao.getName());
 		return es.createEmployee(dao);
 	}
-	@GetMapping(value="/getEmployees")
+
+	@GetMapping(value = "/getEmployees")
 	public List<Employee> getEmployee() {
+		log.info("showing all employees");
 		return es.getAllEmployees();
 	}
-	@GetMapping(value="/getEmployees/{id}")
+
+	@GetMapping(value = "/getEmployees/{id}")
 	public Employee getEmployee(@PathVariable Integer id) {
+		log.info("showing employee with id : " + id);
 		return es.getEmployee(id);
 	}
-	@DeleteMapping(value="/deleteEmployees/{id}")
+
+	@DeleteMapping(value = "/deleteEmployees/{id}")
 	public Employee deleteEmployee(@PathVariable Integer id) {
+		log.warn("deleting employee with id : " + id);
 		return es.getEmployee(id);
 	}
-	@PutMapping(value="/updateEmployees/{id}")
+
+	@PutMapping(value = "/updateEmployees/{id}")
 	public Employee updateEmployee(@PathVariable Integer id, @RequestBody EmployeeDao dao) {
-		System.out.println("Controller updating");
-		return es.updateEmployee(dao,id);
+		log.warn("updating employee with id : " + id + " and name :" + dao.getName());
+		return es.updateEmployee(dao, id);
 	}
 
 }
--- OJT-August8/AugustEight/src/main/java/com/hcl/ojt/service/EmployeeService.java
+++ OJT-August8/AugustEight/src/main/java/com/hcl/ojt/service/EmployeeService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.hcl.ojt.entity.Employee;
 import com.hcl.ojt.entity.EmployeeDao;
 import com.hcl.ojt.repository.EmployeeRepository;
+
 /*Create 
  * Read getEmployee
  * Update 
@@ -16,28 +17,32 @@ import com.hcl.ojt.repository.EmployeeRepository;
 public class EmployeeService {
 	@Autowired
 	EmployeeRepository er;
+
 	public Employee createEmployee(EmployeeDao dao) {
 		Employee em = new Employee();
 		em.setName(dao.getName());
 		em.setSalary(dao.getSalary());
 		return er.save(em);
 	}
+
 	public Employee getEmployee(Integer id) {
 		return er.findById(id).orElse(null);
 	}
+
 	public void deleteEmployee(Integer id) {
 		er.deleteById(id);
 	}
+
 	public List<Employee> getAllEmployees() {
 		return er.findAll();
 	}
+
 	public Employee updateEmployee(EmployeeDao dao, Integer id) {
 		Employee emp = er.findById(id).orElse(null);
-		System.out.println("Updating");
 		emp.setName(dao.getName());
 		emp.setSalary(dao.getSalary());
 		er.save(emp);
 		return emp;
 	}
-	
+
 }
