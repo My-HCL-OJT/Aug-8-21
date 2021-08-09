2bbcf0a86de6527182b38dffe7421d71efbb0925
sahilraoyadav <50744190+sahilraoyadav@users.noreply.github.com>
2021-08-09 12:49:24 -0400

    Added response wrapper

--- OJT-August8/AugustEight/src/main/java/com/hcl/ojt/controller/EmployeeController.java
+++ OJT-August8/AugustEight/src/main/java/com/hcl/ojt/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.hcl.ojt.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.ojt.entity.Employee;
 import com.hcl.ojt.entity.EmployeeDao;
+import com.hcl.ojt.response.ResponseWrapper;
 import com.hcl.ojt.service.EmployeeService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -24,39 +27,41 @@ public class EmployeeController {
 	EmployeeService es;
 
 	@GetMapping(value = "/")
-	public String showEmployees() {
+	public ResponseEntity<ResponseWrapper<String>> showEmployees() {
 		log.trace("showEmployees start page of the app");
-		return "Welcome /createEmployees to create employees, /getEmployees/id to get employee, /deleteEmployees/id to delete employee";
+		return ResponseEntity.ok(new ResponseWrapper<>(false, HttpStatus.OK,
+				"Welcome /createEmployees to create employees, /getEmployees/id to get employee, /deleteEmployees/id to delete employee"));
 	}
 
 	@PostMapping(value = "/createEmployees")
-	public Employee createEmployee(@RequestBody EmployeeDao dao) {
+	public ResponseEntity<ResponseWrapper<Employee>> createEmployee(@RequestBody EmployeeDao dao) {
 		log.info("employee created " + dao.getName());
-		return es.createEmployee(dao);
+		return ResponseEntity.ok(new ResponseWrapper<>(false, HttpStatus.OK, es.createEmployee(dao)));
 	}
 
 	@GetMapping(value = "/getEmployees")
-	public List<Employee> getEmployee() {
+	public ResponseEntity<ResponseWrapper<List<Employee>>> getEmployee() {
 		log.info("showing all employees");
-		return es.getAllEmployees();
+		return ResponseEntity.ok(new ResponseWrapper<>(false, HttpStatus.OK, es.getAllEmployees()));
 	}
 
 	@GetMapping(value = "/getEmployees/{id}")
-	public Employee getEmployee(@PathVariable Integer id) {
+	public ResponseEntity<ResponseWrapper<Employee>> getEmployee(@PathVariable Integer id) {
 		log.info("showing employee with id : " + id);
-		return es.getEmployee(id);
+		return ResponseEntity.ok(new ResponseWrapper<>(false, HttpStatus.OK, es.getEmployee(id)));
 	}
 
 	@DeleteMapping(value = "/deleteEmployees/{id}")
-	public Employee deleteEmployee(@PathVariable Integer id) {
+	public ResponseEntity<ResponseWrapper<Employee>> deleteEmployee(@PathVariable Integer id) {
 		log.warn("deleting employee with id : " + id);
-		return es.getEmployee(id);
+		return ResponseEntity.ok(new ResponseWrapper<>(false, HttpStatus.OK, es.getEmployee(id)));
 	}
 
 	@PutMapping(value = "/updateEmployees/{id}")
-	public Employee updateEmployee(@PathVariable Integer id, @RequestBody EmployeeDao dao) {
+	public ResponseEntity<ResponseWrapper<Employee>> updateEmployee(@PathVariable Integer id,
+			@RequestBody EmployeeDao dao) {
 		log.warn("updating employee with id : " + id + " and name :" + dao.getName());
-		return es.updateEmployee(dao, id);
+		return ResponseEntity.ok(new ResponseWrapper<>(false, HttpStatus.OK, es.updateEmployee(dao, id)));
 	}
 
 }
